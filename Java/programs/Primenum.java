cdc953897a1d4c173a86b95b8661128fe8371da0
Raafiya23 <raafiyasultana03@gmail.com>
2025-11-05 23:17:54 +0530

    feat: added Prime number program

--- Java/programs/Primenum.java
+++ Java/programs/Primenum.java
@@ -6,20 +6,15 @@ public class Primenum {
         System.out.print("Enter a number: ");
         int n = sc.nextInt();
 
-        if (n <= 1) {
-            System.out.println("Not a prime number");
-            return;
-        }
-
-        int i;
-        for (i = 2; i < n; i++) {
+        for (int i = 2; i < n; i++) {
             if (n % i == 0)
                 break;
         }
 
-        if (i == n)
+        if (i == n){
             System.out.println(n + " is a prime number");
-        else
+        }else{
             System.out.println(n + " is not a prime number");
+        }    
     }
 }
