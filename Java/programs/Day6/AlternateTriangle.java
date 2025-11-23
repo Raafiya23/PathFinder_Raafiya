589c35f7c0107766b7beec13e19804b4eeec3d5b
Raafiya23 <raafiyasultana03@gmail.com>
2025-11-23 18:44:25 +0530

    feat: added Alternating 0-1 Triangle Pattern

--- Java/programs/Day6/AlternateTriangle.java
+++ Java/programs/Day6/AlternateTriangle.java
@@ -1,27 +1,18 @@
 import java.util.Scanner;
 
-public class AlternateTriangle{
+public class AlternateTriangle {
     public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-            System.out.print("Enter row: ");
-            int n = sc.nextInt();
-            for (int i = 1; i <= n; i++) {
-                int value;
-                if (i % 2 == 1) {
-                    value = 1;   
-                } else {
-                    value = 0;   
-                }
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter row: ");
+        int n = sc.nextInt();
 
-                for (int j = 1; j <= i; j++) {
-                    System.out.print(value);
-                    if (value == 1) {
-                        value = 0;
-                    } else {
-                        value = 1;
-                    }
-                }
-                System.out.println();
+        for (int i = 1; i <= n; i++) {
+            int value = (i % 2 == 1) ? 1 : 0;  
+            for (int j = 1; j <= i; j++) {
+                System.out.print(value);
+                value = 1 - value;  
             }
+            System.out.println();
+        }
     }
 }
