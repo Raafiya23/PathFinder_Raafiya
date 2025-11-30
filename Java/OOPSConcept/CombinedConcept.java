eef8d02ea9cb639597872bb537c00b3d6a6712c0
Raafiya23 <raafiyasultana03@gmail.com>
2025-11-30 20:10:27 +0530

    feat: added Combined Concepts program

---
+++ Java/OOPSConcept/CombinedConcept.java
@@ -0,0 +1,37 @@
+abstract class Vehicles{
+    final void startsEngine(){
+        System.out.println("Engine Started..");
+    }
+
+    static String getVehicleType(){
+        return "Generic Vehicle";
+    }
+
+    abstract void drive();
+}
+
+class Cars extends Vehicles{
+    @Override
+    void drive(){
+        System.out.println("Drive a car..");
+    }
+}
+
+class Motorcycle extends Vehicles{
+    @Override
+    void drive(){
+        System.out.println("Drive a bike..");
+    }
+}
+
+public class CombinedConcept{
+    public static void main(String[] args) {
+        Cars c1 = new Cars();
+        c1.drive();
+
+        Motorcycle m1 = new Motorcycle();
+        m1.drive(); 
+
+        System.out.println(Vehicles.getVehicleType());
+    }
+}
